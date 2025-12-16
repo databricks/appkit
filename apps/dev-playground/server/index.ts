2b7aa0cab8600ca6c6b669c21f97ee8e8292033b
Pawel Kosiec <pawel.kosiec@databricks.com>
2025-12-16 12:26:58 +0100

    chore: set app port via env

--- apps/dev-playground/server/index.ts
+++ apps/dev-playground/server/index.ts
@@ -3,12 +3,5 @@ import { reconnect } from "./reconnect-plugin";
 import { telemetryExamples } from "./telemetry-example-plugin";
 
 createApp({
-  plugins: [
-    server({
-      port: Number(process.env.DATABRICKS_APP_PORT) || 8001,
-    }),
-    reconnect(),
-    telemetryExamples(),
-    analytics({}),
-  ],
+  plugins: [server(), reconnect(), telemetryExamples(), analytics({})],
 });
