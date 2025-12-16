b5c143ce2319c4cc1059097f1c4e3c447023ed73
Pawel Kosiec <pawel.kosiec@databricks.com>
2025-12-16 12:39:03 +0100

    chore: fix local dev setup (`pnpm dev`) (#20)

--- apps/dev-playground/client/vite.config.ts
+++ apps/dev-playground/client/vite.config.ts
@@ -14,6 +14,11 @@ export default defineConfig({
       autoCodeSplitting: process.env.NODE_ENV !== "development",
     }),
   ],
+  server: {
+    hmr: {
+      port: 24679,
+    },
+  },
   optimizeDeps: {
     include: [
       "react",
