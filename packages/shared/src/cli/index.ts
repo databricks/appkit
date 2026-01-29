302cbd776f276c8002cd604cb2a51929509203e8
Pawel Kosiec <pawel.kosiec@databricks.com>
2026-01-29 13:23:43 +0100

    chore: include version in the CLI

--- packages/shared/src/cli/index.ts
+++ packages/shared/src/cli/index.ts
@@ -1,14 +1,24 @@
 #!/usr/bin/env node
 
+import { readFileSync } from "node:fs";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
 import { Command } from "commander";
 import { setupCommand } from "./commands/setup.js";
 import { generateTypesCommand } from "./commands/generate-types.js";
 import { lintCommand } from "./commands/lint.js";
 import { docsCommand } from "./commands/docs.js";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const pkgPath = join(__dirname, "../../package.json");
+const pkg = JSON.parse(readFileSync(pkgPath, "utf-8"));
+
 const cmd = new Command();
 
-cmd.name("appkit").description("CLI tools for Databricks AppKit");
+cmd
+  .name("appkit")
+  .description("CLI tools for Databricks AppKit")
+  .version(pkg.version);
 
 cmd.addCommand(setupCommand);
 cmd.addCommand(generateTypesCommand);
