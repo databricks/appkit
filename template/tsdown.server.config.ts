be0155b721e9f11e1af413c34148b07fdda8cdb4
MarioCadenas <MarioCadenas@users.noreply.github.com>
2026-02-26 17:54:51 +0100

    chore: add tsdown to template

---
+++ template/tsdown.server.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'tsdown';
+
+export default defineConfig({
+  entry: 'server/server.ts',
+  unbundle: true,
+  external: (id) => /^[^./]/.test(id) || id.includes('/node_modules/'),
+  tsconfig: 'tsconfig.server.json',
+});
