e38060ae5322d71ef479c13fc38e1cee87eb965f
MarioCadenas <MarioCadenas@users.noreply.github.com>
2026-04-20 19:31:29 +0200

    refactor: split codemod into separate PR
    Remove the codemod CLI from this PR to keep the review focused
    on the core lifecycle change. The codemod will land as a follow-up
    with bug fixes from review.
    
    Runtime detection (constructor autoStart throw + exports().start()
    trap) stays since it's part of the migration story.
    
    Signed-off-by: MarioCadenas <MarioCadenas@users.noreply.github.com>

--- packages/shared/src/cli/index.ts
+++ packages/shared/src/cli/index.ts
@@ -4,7 +4,6 @@ import { readFileSync } from "node:fs";
 import { dirname, join } from "node:path";
 import { fileURLToPath } from "node:url";
 import { Command } from "commander";
-import { codemodCommand } from "./commands/codemod/index.js";
 import { docsCommand } from "./commands/docs.js";
 import { generateTypesCommand } from "./commands/generate-types.js";
 import { lintCommand } from "./commands/lint.js";
@@ -27,6 +26,5 @@ cmd.addCommand(generateTypesCommand);
 cmd.addCommand(lintCommand);
 cmd.addCommand(docsCommand);
 cmd.addCommand(pluginCommand);
-cmd.addCommand(codemodCommand);
 
 cmd.parse();
